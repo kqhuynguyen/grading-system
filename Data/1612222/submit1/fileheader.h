2f7b4ba11ea5a676eb8da225000ca7a20cf53b79
trungnhanuchiha <33752801+trungnhanuchiha@users.noreply.github.com>
2017-11-28 02:32:21 +0700

    add some folder
    add some folder

--- Data/1612222/submit1/fileheader.h
+++
@@ -1,7 +0,0 @@
-#include<string>
-using namespace std;
-#ifndef fileheader_h
-#define fileheader_h
-void readFileData(string filenamein,float *arr,int &dem);
-void process(string filenameout,float *arr, int n);
-#endif // !fileheader.h
